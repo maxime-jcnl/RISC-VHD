3a86c7f91ce32bab9c1b3072c9ab1fb5c7ae8924
18nelli <maxime.jaconelli@efrei.net>
2026-05-29 16:39:57 +0200

    uc updated

--- src/uc.vhd
+++ src/uc.vhd
@@ -34,7 +34,7 @@ entity uc is
         AddSub : out STD_LOGIC;
         G_in : out STD_LOGIC;
         -- Signal de fin d'exécution de l'instruction
-        done : out STD_LOGIC;
+        done : out STD_LOGIC
     );
 end entity uc;
 
@@ -50,8 +50,8 @@ Architecture Behavioral of uc is
     begin
         -- Décodage de l'instruction IIIXXXYYY
         instruction <= IR(8 downto 6); -- III : opération
-        rx <= IR(5 downto 3); -- XXX : Registre source
-        ry <= IR(2 downto 0); -- YYY : Registre destination
+        rx <= IR(5 downto 3); -- XXX : Registre destination
+        ry <= IR(2 downto 0); -- YYY : Registre source
 
         -- Processus de séquence pour la machine à états
         process(clk, rst)
@@ -60,7 +60,6 @@ Architecture Behavioral of uc is
                     current_state <= S0;
                 elsif rising_edge(clk) then
                     current_state <= next_state;
-                    end if;
                 end if;
         end process;
 
@@ -113,5 +112,137 @@ Architecture Behavioral of uc is
                                 next_state <= S3_4_1;
                             when others =>
                                 next_state <= S0; -- Instruction non reconnue, revenir à l'état initial
-                    
-
+                        end case;
+                    when S3_1 => -- MOV Rx, Ry
+                        case ry is
+                            when "000" => R0_out <= '1';
+                            when "001" => R1_out <= '1';
+                            when "010" => R2_out <= '1';
+                            when "011" => R3_out <= '1';
+                            when "100" => R4_out <= '1';
+                            when "101" => R5_out <= '1';
+                            when "110" => R6_out <= '1';
+                            when "111" => R7_out <= '1';
+                            when others => null;
+                        end case;
+                        case rx is
+                            when "000" => R0_in <= '1';
+                            when "001" => R1_in <= '1';
+                            when "010" => R2_in <= '1';
+                            when "011" => R3_in <= '1';
+                            when "100" => R4_in <= '1';
+                            when "101" => R5_in <= '1';
+                            when "110" => R6_in <= '1';
+                            when "111" => R7_in <= '1';
+                            when others => null;
+                        end case;
+                        done <= '1'; -- Instruction terminée
+                        next_state <= S0; -- Revenir à l'état initial
+                    when S3_2 => -- MVI Rx, #D
+                        case rx is
+                            when "000" => R0_in <= '1';
+                            when "001" => R1_in <= '1';
+                            when "010" => R2_in <= '1';
+                            when "011" => R3_in <= '1';
+                            when "100" => R4_in <= '1';
+                            when "101" => R5_in <= '1';
+                            when "110" => R6_in <= '1';
+                            when "111" => R7_in <= '1';
+                            when others => null;
+                        end case;
+                        DIN_out <= '1'; -- Charger sur le bus la donnée immédiate D
+                        done <= '1'; -- Instruction terminée
+                        next_state <= S0; -- Revenir à l'état initial
+                    when S3_3_1 => -- ADD Rx, Ry
+                        case rx is
+                            when "000" => R0_out <= '1';
+                            when "001" => R1_out <= '1';
+                            when "010" => R2_out <= '1';
+                            when "011" => R3_out <= '1';
+                            when "100" => R4_out <= '1';
+                            when "101" => R5_out <= '1';
+                            when "110" => R6_out <= '1';
+                            when "111" => R7_out <= '1';
+                            when others => null;
+                        end case;
+                        A_in <= '1'; -- Charger Rx dans le registre A
+                        next_state <= S3_3_2;
+                    when S3_3_2 =>
+                        case ry is
+                            when "000" => R0_out <= '1';
+                            when "001" => R1_out <= '1';
+                            when "010" => R2_out <= '1';
+                            when "011" => R3_out <= '1';
+                            when "100" => R4_out <= '1';
+                            when "101" => R5_out <= '1';
+                            when "110" => R6_out <= '1';
+                            when "111" => R7_out <= '1';
+                            when others => null;
+                        end case;
+                        AddSub <= '0'; -- Sélectionner l'opération d'addition
+                        G_in <= '1'; -- Charger le résultat de l'addition dans le registre G
+                        next_state <= S3_3_3;
+                    when S3_3_3 =>
+                        case rx is
+                            when "000" => R0_in <= '1';
+                            when "001" => R1_in <= '1';
+                            when "010" => R2_in <= '1';
+                            when "011" => R3_in <= '1';
+                            when "100" => R4_in <= '1';
+                            when "101" => R5_in <= '1';
+                            when "110" => R6_in <= '1';
+                            when "111" => R7_in <= '1';
+                            when others => null;
+                        end case;
+                        G_out <= '1'; -- Sortir le résultat de l'addition sur le bus
+                        done <= '1'; -- Instruction terminée
+                        next_state <= S0; -- Revenir à l'état initial
+                    when S3_4_1 => -- SUB Rx, Ry
+                        case ry is
+                            when "000" => R0_out <= '1';
+                            when "001" => R1_out <= '1';
+                            when "010" => R2_out <= '1';
+                            when "011" => R3_out <= '1';
+                            when "100" => R4_out <= '1';    
+                            when "101" => R5_out <= '1';
+                            when "110" => R6_out <= '1';
+                            when "111" => R7_out <= '1';
+                            when others => null;
+                        end case;
+                        A_in <= '1'; -- Charger Rx dans le registre A
+                        next_state <= S3_4_2;
+                    when S3_4_2 =>
+                        case rx is
+                            when "000" => R0_out <= '1';
+                            when "001" => R1_out <= '1';
+                            when "010" => R2_out <= '1';
+                            when "011" => R3_out <= '1';
+                            when "100" => R4_out <= '1';
+                            when "101" => R5_out <= '1';
+                            when "110" => R6_out <= '1';
+                            when "111" => R7_out <= '1';
+                            when others => null;
+                        end case;
+                        AddSub <= '1'; -- Sélectionner l'opération de soustraction
+                        G_in <= '1'; -- Charger le résultat de la soustraction dans le registre G
+                        next_state <= S3_4_3;
+                    when S3_4_3 =>
+                        case rx is
+                            when "000" => R0_in <= '1';
+                            when "001" => R1_in <= '1';
+                            when "010" => R2_in <= '1';
+                            when "011" => R3_in <= '1';    
+                            when "100" => R4_in <= '1';
+                            when "101" => R5_in <= '1';
+                            when "110" => R6_in <= '1';
+                            when "111" => R7_in <= '1';
+                            when others => null;
+                        end case;
+                        G_out <= '1'; -- Sortir le résultat de la soustraction sur le bus
+                        done <= '1'; -- Instruction terminée
+                        next_state <= S0; -- Revenir à l'état initial
+                    when others =>
+                        next_state <= S0; -- Revenir à l'état initial pour tout état non défini
+                end case;
+            end process;
+end architecture Behavioral;
