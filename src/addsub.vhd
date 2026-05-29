a6dad4cb1e11018d4fea681ff8f1152a9909fd9d
18nelli <maxime.jaconelli@efrei.net>
2026-05-29 13:18:04 +0200

    addsub entity added

---
+++ src/addsub.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_NUMERIC_STD.ALL;
+
+entity addsub is
+    Port (
+        x : in STD_LOGIC_VECTOR(8 downto 0);
+        y : in STD_LOGIC_VECTOR(8 downto 0);
+        AddSub : in STD_LOGIC;
+        result : out STD_LOGIC_VECTOR(8 downto 0)
+    );
+end entity addsub;
+
+Architecture Dataflow of addsub is
+    begin 
+    result <= std_logic_vector(signed(x) + signed(y)) when AddSub = '0' else
+              std_logic_vector(signed(x) - signed(y));
+end architecture Dataflow;
